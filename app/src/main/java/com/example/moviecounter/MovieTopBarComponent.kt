e183a7603509de5f39e0ba9a29fdbdc9b476286a
Yuri <yuri.mamani.m@tecsup.edu.pe>
2026-04-12 20:20:26 -0500

    feat: add TopAppBar component

---
+++ app/src/main/java/com/example/moviecounter/MovieTopBarComponent.kt
@@ -0,0 +1,16 @@
+package com.example.moviecounter
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MovieTopBarComponent() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(text = "MovieCounter")
+        }
+    )
+}
